3cc7cad2e768a70e7449db694661bd76ee09371d
Andreia Gomes Marins <131017177+andreiagomes0906@users.noreply.github.com>
2024-02-28 17:53:53 -0300

    Add files via upload
    Meu primeiro desafio de Componente funcional.

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Olá, este é o meu primeiro desafio sobre componente funcional!</h1>
+      <div className="card">
+        
+        
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
